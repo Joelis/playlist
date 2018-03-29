a0515ae4a125cd0e4157f563743b823ac3264354
Joelis Paula <jpaula@cristoreyny.org>
2018-03-29 12:39:39 +0000

    added new songs

--- js/script.js
+++ js/script.js
@@ -17,30 +17,32 @@
 
 // Songs
 var mySong = {
-	"title":"24K Magic",
-	"artist":"Bruno Mars",
-	"mp3-url":"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
-	"image-url":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
+	"title":"All Day",
+	"artist":"G Herbo ft. Blac Youngsta",
+	"video":"https://www.youtube.com/watch?v=oRQO7IS0meM",
+	"picture":"http://s3.amazonaws.com/hiphopdx-production/2017/09/g-herbo-album-cover-630x629.jpg",
+	songLength: "4 minutes",
+	songGenre: "Trap",
 }
 
 var myPlayList = [
 	{
-		"title":"24K Magic",
-		"artist":"Bruno Mars",
-		"mp3-url":"https://open.spotify.com/track/6b8Be6ljOzmkOmFslEb23P",
-		"image-url":"https://images-na.ssl-images-amazon.com/images/I/71Gr9aCHQfL._SY355_.jpg",
+		"title":"I Need That",
+		"artist":"Lil Reese",
+		"https://www.youtube.com/watch?v=dsWgUDSszII",
+		"image-url":"http://hw-static.worldstarhiphop.com/u/pic/2013/08/70701f4.jpg",
 	},
 	{
-		"title":"Sir Duke",
-		"artist":"Stevie Wonder",
-		"mp3-url":"https://open.spotify.com/track/2udw7RDkldLFIPG9WYdVtT",
-		"image-url":"https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/Songs_in_the_key_of_life.jpg/220px-Songs_in_the_key_of_life.jpg",
+		"title":"Fashion",
+		"artist":"Jay Critch ft. Rich the Kid",
+		"mp3-url":"https://www.youtube.com/watch?v=ZBT7yXr-hDY",
+		"image-url":"https://i.ytimg.com/vi/ZBT7yXr-hDY/maxresdefault.jpg",
 	},
 	{
-		"title":"Sorry",
-		"artist":"Justin Bieber",
-		"mp3-url":"https://open.spotify.com/track/09CtPGIpYB4BrO8qb1RGsF",
-		"image-url":"http://assets-s3.usmagazine.com/uploads/assets/articles/93827-justin-biebers-sorry-choreographer-spills-video-style-secrets-parris-goebel/1445638548_justin-bieber-sorry-dancers-zoom.jpg",
+		"title":"Walk it Talk it",
+		"artist":"Migos",
+		"mp3-url":"https://play.google.com/music/preview/T5vbjfcib5gknrnix4qqranrwy4?play=1",
+		"image-url":"https://cdn.rappersroom.com/wp-content/uploads/2018/03/migos-walk-it-talk-it-drake.jpg",
 	}
 
 ]
@@ -49,8 +51,8 @@ var myPlayList = [
 
 // DOCUMENT READY FUNCTION
 $( document ).ready(function() {
-  
-
+  $('.songs').append("<p>Company name: " + classroom.companyName + "</p>");
+$("body").append("<h2>" + myObject.songGenre + "</h2>");
 
 
 });
